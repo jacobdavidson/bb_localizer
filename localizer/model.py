d82ca69339d2e68fbe01ef640b6931e8ae7d441a
Benjamin Wild <b.w@fu-berlin.de>
2019-09-26 15:50:56 +0200

    Update model definition

--- localizer/model.py
+++ localizer/model.py
@@ -1,4 +1,4 @@
-from keras.layers import Input
+from keras.layers import Input, Lambda
 from keras.models import Model
 from keras.layers.core import Dropout, Flatten, SpatialDropout2D
 from keras.layers.normalization import BatchNormalization
@@ -8,30 +8,39 @@ from keras.layers.convolutional import Conv2D
 def get_conv_model(initial_channels=32):
     inputs = Input(shape=(None, None, 1))
 
-    x = Conv2D(initial_channels, (5, 5), strides=(2, 2), activation='relu')(inputs)
-    x = SpatialDropout2D(.1)(x)
-    x = Conv2D(initial_channels, (3, 3), strides=(1, 1), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = SpatialDropout2D(.1)(x)
-    x = Conv2D(initial_channels, (3, 3), strides=(2, 2), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = SpatialDropout2D(.1)(x)
-    x = Conv2D(initial_channels * 2**1, (3, 3), strides=(1, 1), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = SpatialDropout2D(.1)(x)
-    x = Conv2D(initial_channels * 2**1, (3, 3), strides=(2, 2), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(initial_channels * 2**2, (3, 3), strides=(1, 1), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(initial_channels * 2**3, (3, 3), strides=(2, 2), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(initial_channels * 2**3, (3, 3), strides=(1, 1), activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(initial_channels * 2**3, (3, 3), strides=(1, 1), activation='relu')(x)
-    x = Dropout(.5)(x)
-    x = Conv2D(4, (1, 1), activation='sigmoid')(x)
+    x_0 = Conv2D(initial_channels, (3, 3), strides=(2, 2), activation='relu')(inputs)
+    x_0 = SpatialDropout2D(.1)(x_0)
 
-    model = Model(inputs=inputs, outputs=x)
+    x_1 = Conv2D(initial_channels * 2 ** 1, (3, 3), strides=(2, 2), activation='relu')(x_0)
+    x_1 = BatchNormalization()(x_1)
+    x_1 = SpatialDropout2D(.1)(x_1)
+
+    x_2 = Conv2D(initial_channels * 2 ** 2, (3, 3), strides=(2, 2), activation='relu')(x_1)
+    x_2 = BatchNormalization()(x_2)
+    x_2 = SpatialDropout2D(.1)(x_2)
+
+    x_2 = Conv2D(initial_channels * 2 ** 2, (3, 3), strides=(1, 1), activation='relu')(x_2)
+    x_2 = BatchNormalization()(x_2)
+    x_2 = SpatialDropout2D(.1)(x_2)
+
+    x_2 = Conv2D(initial_channels * 2 ** 2, (3, 3), strides=(1, 1), activation='relu')(x_2)
+    x_2 = BatchNormalization()(x_2)
+    x_2 = SpatialDropout2D(.1)(x_2)
+
+    x_2 = Conv2D(initial_channels * 2 ** 2, (3, 3), strides=(1, 1), activation='relu')(x_2)
+    x_2 = BatchNormalization()(x_2)
+    x_2 = SpatialDropout2D(.1)(x_2)
+
+    x_2 = Conv2D(initial_channels * 2 ** 2, (3, 3), strides=(1, 1), activation='relu')(x_2)
+    x_2 = BatchNormalization()(x_2)
+
+    x_3 = Conv2D(initial_channels * 2 ** 3, (3, 3), strides=(1, 1), activation='relu')(x_2)
+    x_3 = BatchNormalization()(x_3)
+
+    xu_3 = Conv2D(initial_channels, (1, 1), strides=(1, 1), activation='relu')(x_3)
+    xu_3 = Conv2D(4, (1, 1), activation='sigmoid')(xu_3)
+
+    model = Model(inputs=inputs, outputs=xu_3)
 
     return model
 
@@ -41,7 +50,7 @@ def get_train_model(initial_channels=32, model_factory=get_conv_model):
 
     inputs = Input(shape=(128, 128, 1))
     x = conv_model(inputs)
-    x = Flatten()(x)
+    x = Lambda(lambda t: t[:, 2, 2, :])(x)
 
     model = Model(inputs=inputs, outputs=x)
 
